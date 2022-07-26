a96838cba4a77318e2b6d44b46223d8405219be1
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-07-26 20:34:16 +0200

    started adding firebase

--- blockchain.js
+++ blockchain.js
@@ -1,6 +1,10 @@
 const Block = require('./block');
 const crypto = require('crypto');
 const Swarm =  require('@erebos/swarm-node');
+const firebaseApp = require('firebase/app');
+var http = require('http');
+const firestore = require('firebase/firestore');
+//import { initializeApp } from 'firebase/app';
 
 
 class BlockChain {
@@ -14,7 +18,8 @@ class BlockChain {
 		this.swarm = new Swarm.SwarmClient({
 			bzz: { url: 'http://localhost:8500' },
 		});
-		  
+        this.firebaseInit();
+        this.isNodeAlreadyInDB();
 	};
 	createFirstBlock() {
 		return new Block(0,Date.now(),{pow: 9,transactions: []}, '0');
@@ -50,6 +55,40 @@ class BlockChain {
         }
 		return incrementor;
 	}
+    firebaseInit(){
+        this.firebaseConfig = {
+            apiKey: "AIzaSyASk7Y85L53MT2eW4CIG6SkWVJJenZ1F40",
+            authDomain: "blockchain-111e9.firebaseapp.com",
+            projectId: "blockchain-111e9",
+            storageBucket: "blockchain-111e9.appspot.com",
+            messagingSenderId: "764706009415",
+            appId: "1:764706009415:web:19dc1951d10ebe479c5c62"
+        };    
+        this.app = firebaseApp.initializeApp(this.firebaseConfig);
+        this.db = firestore.getFirestore(this.app);
+    }
+
+    async getIp(){
+        global.ipForReturn = "";
+        http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+            resp.on('data', function(ip) {
+              console.log("My public IP address is: " + ip);
+              ipForReturn = ip;
+            });
+        });
+        return ipForReturn;
+    }
+    async isNodeAlreadyInDB(){
+        var ip = "";
+        ip = await this.getIp()
+        const q = firestore.query(firestore.collection(this.db, "nodes"), firestore.where("ip", "==", ip));
+        const querySnapshot = await firestore.getDocs(q);
+
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+        });
+    }
+
 }
 
 
